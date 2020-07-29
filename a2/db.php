4d6d1e6fd275da76440fb0562004fbfffdf7f6b8
elhadinata <35050260+elhadinata@users.noreply.github.com>
2020-07-29 22:38:21 +1000

    ass2

---
+++ a2/db.php
@@ -0,0 +1,31 @@
+<HTML>
+<HEAD>
+<TITLE>CGIWrap Error: Execution of this script not permitted</TITLE>
+<CENTER><H2>CGIWrap Error: Execution of this script not permitted</H2></CENTER>
+<HR><p></HEAD><BODY>
+Execution of (19s1/pracs/06/db.php.txt) is not permitted
+for the following reason:
+
+<P><DL><DT>Script is not executable. Issue 'chmod 755 filename'</DL>
+<P>
+<DL>
+<DT><B>Server Data:</B>
+<P>
+<DD><B>Server Administrator/Contact</B>: ss@cse.unsw.edu.au
+<DD><B>Server Name</B>: cgi.cse.unsw.edu.au
+<DD><B>Server Port</B>: 443
+<DD><B>Server Protocol</B>: HTTP/1.1
+<DD><B>Virtual Host</B>: cgi.cse.unsw.edu.au
+</DL>
+<P>
+<DL>
+<DT><B>Request Data:</B>
+<P>
+<DD><B>User Agent/Browser</B>: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36
+<DD><B>Request Method</B>: GET
+<DD><B>Remote Address</B>: 172.26.15.186
+<DD><B>Remote Port</B>: 50574
+<DD><B>Referring Page</B>: https://cgi.cse.unsw.edu.au/~cs3311/19s1/pracs/06/index.php
+</DL>
+
+</BODY></HTML>
